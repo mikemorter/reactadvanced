044ded834c00b849c1649057eb74a246618971d0
mikemorter <mikemorter@gmail.com>
2025-02-03 10:15:51 -0700

    format app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
 
 class App extends Component {
+  handleLoggedIn = (username) => {
+    console.log("getting the user: " + username);
+    const user = { name: "Mike" };
+    this.setState({ currentUseer: user });
+  };
+
   state = { currentUseer: { name: "Mike" } };
 
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUseer}>
+      <UserContext.Provider
+        value={{
+          currentUser: this.state.currentUseer,
+          onLoggedIn: this.handleLoggedIn,
+        }}
+      >
         <div>
           <MoviePage />
         </div>
